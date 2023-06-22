ac1f0c53bafc0ca1654e5bf31402e36acfcd08bb
AirplaneGoBrr <mottlcompound@gmail.com>
2023-06-22 00:17:36 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "cookies": `x-amzn-dat-gui-client-v=1.24.208784.0;session-id=136-8796888-2363403;ubid-main=134-8750339-4318526;session-id-time=2318132522l;session-token=doIv1YWFeU7lmObbDr/3lipmYlOLNSOrdwg53U/MKDdtiYp1Hv5xQUgJh6ZGjfST+I3uZWKzdZqqKmHNEsrRztym4OFS1WxSQmYSpBa1onpt1CL6sUfKjFqC3COY4uJcNI0Ys8EvmvywYBhroB45x6D5KnPrMTc1+Lvh1u24dK32BSgQRnryjJGnvhPcZstdPdhdMSe2CAaqWWOiAQydLgl2kHagD8FZEmvNI3NLqdCvzbrFnz6A+08xSvZZmJSR;x-main="pRyY8qKNACfHv2ltiuao21ZL51cxVQ6z0vBa2tCfcXG@ges5zGTKA@cFLvow4GSD";at-main=Atza|IwEBIPF4YMOUeG4ijLWGntR0Fvc06ja94jCsu-wOpFGd4ajmmRHzkNbZak24n_UdmcTnXLrr4n52GCc8EAeLF7WM2pS2xmW-CWIeJVNxv5_c39p9L7NdriGQAG-rmGHhurD3xRb9SLqLJ7WrLoo3bK2MNVOuRTgk9kjcOfuii-JVdFGibGuHiavDF19ZYY-Oqd2sv_oeNGEtsmuNIHGqk9LokbuoJ4Q6uMcXvQ5JNYryQaB4bA;sess-at-main="mcy2OnZhgiqIB0UbznWJVy/1pJ4T0XcVuIbA7eqQXZE=";csrf=-630944986;`,
-    "domain": "amazon.com"
-}
