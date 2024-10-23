c665262174b58f7d99fcca71cdaa34951ed453d0
AirplaneGoBrr <mottlcompound@gmail.com>
2024-10-22 19:48:01 -0700

    🐛 Bug fix + ➕ Added URL decoding to cookie input string

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # alexa-smart-plug-API
 
-(Some code was taken from MeemeeLab [project](https://github.com/MeemeeLab/alexa-smart-plug))
+(Most* code was taken from MeemeeLab [project](https://github.com/MeemeeLab/alexa-smart-plug), huge shout out to them for their work!)
 
 ## What is this
 
-This is a simple alexa smart plug controller
+This is a simple alexa smart plug controller.
 
-You can turn on and off your smart plugs
+You can turn on and off your smart plugs.
 
 As well as get the current status!
 
@@ -18,15 +18,22 @@ const SmartPlugBuilder = require("./smartplug")
 const smartplug = new SmartPlugBuilder(cookies, domain);
 ```
 
-2. Get your alexa cookies
-You can do this by going to the alexa website `https://alexa.amazon.com/spa/index.html` open devtools and goto the networking tab and refresh your page and you will see the cookie box, copy that and goto a URL decoder and decode the string and then put it back in the code
+2. Get your alexa cookies, Amazon has removed the Alexa website (Thanks, Amazon.) So you have to use an API route to get it, I would use [https://alexa.amazon.com/api/phoenix](https://alexa.amazon.com/api/phoenix) the next steps are all the same.
 
+Also there is a step talking about using a URL decoder- that can be ignored as this module will decode the cookie string for you.
+
+**Keep in mind, cookies do expire! You will have to update your cookie string every so offen**
+
+Note: if you have the `raw` option (like firefox does) enable it and copy the cookie, you can even include the `Cookie: ` part and the module will replace it for you.
 ![Step1](imgs/s1.png)
+
+The next step can be **ignored** as the module will decode it for you!
+
 ![Step2](imgs/s2.png)
 
-3. Put all the cookie info as the first arg and your amazon URL as the 2ed arg (not sure? just put `amazon.com` ) make sure to remove any subdomains or paths from the domain
+1. Put the whole cookie string as the first arg and your Amazon URL as the 2ed arg (not sure what url? just put `amazon.com` ) make sure to remove any subdomains or paths from the domain
 
-4. Have fun
+2. Have fun
 ```js
 const SmartPlugBuilder = require("./smartplug")
 const { cookies, domain } = require("./config")
@@ -45,3 +52,13 @@ const smartplug = new SmartPlugBuilder(cookies, domain);
     console.log(state) // Output: false (the light is off!)
 })();
 ```
+
+# List of Amazon domains
+
+[https://gist.github.com/AminulBD/8c347539ecd49a8ab0b24544dd2ebab1](https://gist.github.com/AminulBD/8c347539ecd49a8ab0b24544dd2ebab1)
+
+make sure its just the domain name, don't include `https://` or `www`
+
+# Todo
+
+-- N/A --
--- smartplug.js
+++ smartplug.js
@@ -1,5 +1,4 @@
-const { default: axios } = require("axios");
-const fs = require("fs")
+const axios = require("axios").default;
 
 function parseJSONStrings(input) {
     let obj;
@@ -32,6 +31,9 @@ class SmartPlug {
         if (!cookie) {
             throw new Error('No cookie provided.');
         }
+        // Cookie: session-id=143-0721757-2562658; session-id-time=2082787201l; ubid-main=133-3854304-2639966; sst-main=Sst1|PQE4FEMD1KJnFnU322ZDbq7rCa9tF-LJ4ORrq51lYW_iLyQHh7lQihs_YaudOEf2zpeVeuAAFgylN-41HacaQ56jAa2ZDcaMl_VtbxJ6pP4paTphgZN2HYDX7H5Ersy0HSTzLv1-1jJU_J9YUqv_91TeIEE-4PW7ZNYn4nn1jBHYTdVCrI7MVrPFXyqM3m827Y7IaTFVay66GdFw4UnAADYi7osln8rSa0rL7s3dgLwYVyih76YOIVizBNPOi1d5q7WG9pjjhRi89zzC95EwMyWwG5DPKVgO4GCrRbrFi6Q1d0E; AMCV_7742037254C95E840A4C98A6%40AdobeOrg=1585540135%7CMCIDTS%7C19880%7CMCMID%7C05284008687270434080342980655488712239%7CMCAAMLH-1718222586%7C9%7CMCAAMB-1718222586%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1717624986s%7CNONE%7CMCSYNCSOP%7C411-19887%7CMCAID%7CNONE%7CvVersion%7C4.4.0; aws-target-visitor-id=1702275292874-590486.45_0; aws-target-data=%7B%22support%22%3A%221%22%7D; regStatus=pre-register; aws-ubid-main=621-1065477-3684365; s_nr=1708838296883-Repeat; s_vnum=2136319751658%26vn%3D5; s_dslv=1708838296883; session-token=/jCinICHVDcyt0CuSYcXxVuLdBMWRInBYXmYL8b4Y8ZBoa6E0d1O/NMvpg0QUJcoPV094sYTR5DSoeR8zdCnQrLa/BrX+VpGAQHvGcJaGzRJcDluWKLHstB9rlgqUmUJB0OnsJ4oneY41reRxDLS2BERU6h84WS/py9OblDEsCJvcQpY8nyayPp8xNOavmnJyg85z3+D/9x3NRCaaAktpUd/ff8S1EtaVZPc8G4tgOK57hgkgd/KpxfrN0CN2LB4TCcCtLdRpkSIj0dq+rjRaqu/B151Yexdffz17ygIypTYX8f9qFXA92KW4YzZRwmuWa64R0k5P62OMGNisZxamEErdPbx6KwoKU4p66qGzAFOlX5PA/daOpSSBuppCuTf; lc-main=en_US; i18n-prefs=USD; x-main=A8J2o3GPtu4wdasjaLqepUdl82UghObgyIF9gD7nQVo@FDfHtX0m3Ss?akrNMKlw; at-main=Atza|IwEBICZT3xwrTDWQjrj-8vdZ9tBss6nrxp0j9_CmF8aJbgVs4taisADX-j0FFkJhe2wjDAQAuwIkn26Dtg3nT9C2bpw0KCmihIxFh7BAosFmo0Fa1puteGIGJEtnS9G9lHLx4Miwu8q867Fx5dfyOOlHYfX-vqVJ5CM93arr_G3RdQcP7plrzxHkZjek-FEhVkWhqh7QKIC0BwEhpTicYBxZsZHCnjFKKP4Db9CGQIPpqYrRpg; sess-at-main="U2jl9sBSpAcCcJopSiQ4yaZXxUrSDwrijZhgVBsfP98="; s_cc=true; s_sq=%5B%5BB%5D%5D; s_ppv=0; skin=noskin; aws_lang=en; AMCVS_7742037254C95E840A4C98A6%40AdobeOrg=1; aws-mkto-trk=id%3A112-TZM-766%26token%3A_mch-aws.amazon.com-1708736752213-59782
+        cookie = decodeURI(cookie.replace("Cookie: ", "").split("\n").shift()) // Replace `Cookie: `, new lines and decode string
+
         this._cookie = cookie;
 
         if (!amazonDomain) {
@@ -124,8 +126,11 @@ class SmartPlug {
             }
         })
 
-        const capabilityStates = response.data.deviceStates[0].capabilityStates.map(v => JSON.parse(v));
-        return capabilityStates.find(v => v.namespace === 'Alexa.PowerController').value === 'ON' ? true : false;
+        if (response.data?.deviceStates[0]?.capabilityStates) {
+            const capabilityStates = response.data.deviceStates[0].capabilityStates.map(v => JSON.parse(v));
+            return capabilityStates.find(v => v.namespace === 'Alexa.PowerController').value === 'ON';
+        }
+        return null;
     }
 
 
